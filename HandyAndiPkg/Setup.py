28c3dae5ea7224f2379bccdebc11c7c0f85a54d3
AndileMasi <48668961+AndileMasi@users.noreply.github.com>
2019-03-22 15:38:52 +0200

    Update on the file name

--- HandyAndiPkg/Setup.py
+++ HandyAndiPkg/Setup.py
@@ -11,7 +11,7 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='Handy Andi is a project that Andi created for school.',
+    description='Handy Andi is a python project that Andi created for school.',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
     url='https://github.com/AndileMasi/HandyAndyPackage',
